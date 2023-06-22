f9cfc3fe22b3c59942ede58e9727bdaf643cf056
Seolmin <drhon10@gmail.com>
2023-06-23 07:14:55 +0900

    Add

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,8 +11,10 @@ public class Player : MonoBehaviour
     bool jDown;
 
     bool isJump;
+    bool isDodge;
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
     Rigidbody rigid;
 
@@ -35,6 +37,7 @@ public class Player : MonoBehaviour
         Move();
         Turn();
         Jump();
+        Dodge();
     }
 
     void GetInput()
@@ -49,6 +52,11 @@ public class Player : MonoBehaviour
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
+        if(isDodge)
+        {
+            moveVec = dodgeVec; 
+        }
+
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
@@ -62,13 +70,32 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        if(jDown && !isJump)
+        if(jDown && moveVec == Vector3.zero && !isJump && !isDodge)
         {
             rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
             anim.SetBool("isJump", true);
             anim.SetTrigger("doJump");
             isJump = true;
         }
+    }
+
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.5f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
     }
 
     void OnCollisionEnter(Collision collision)
